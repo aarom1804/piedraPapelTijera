ca99f5346dfd2eb2f09f2b3091e6cbd9f90bb961
Jeynner <jeynnerg@gmail.com>
2025-03-24 23:05:01 +0100

    Ejercicio terminado, aunque faltaria agregar un limite de juego

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let arrayOption = ["👊", "🤚", "✌"];
+let arrayOption = ["👊", "🤚", "✌️"];
 let randomValue;
 let valueSelected;
 let valueComputer;
@@ -7,6 +7,7 @@ let pointComputer = 0;
 let computerIntervalId = setInterval(updateOutputComputer, 333);
 let indexOutput = 0;
 let draw = "empate";
+let clean = "";
 
 function random(){
     randomValue = Math.floor(Math.random()*3);
@@ -18,28 +19,27 @@ function optionSelected(){
     clearInterval(computerIntervalId);
     valueComputer = arrayOption[random()];
     document.getElementById("valueComputer").innerText = valueComputer;
-    if(valueSelected == valueComputer){
-        document.getElementById("text").innerText = draw;
+    if(valueSelected == valueComputer){        
         setTimeout(function() {
             computerIntervalId = setInterval(updateOutputComputer, 333);
-            
         }, 1500);
-        resetRadio();
-    }else if((valueSelected == "👊" && valueComputer == "✌")||(valueSelected == "✌" && valueComputer == "🤚")||(valueSelected == "🤚" && valueComputer == "👊")){
+        setTimeout(resetRadio, 1500);
+        document.getElementById("text").innerText = draw;
+        
+        return setTimeout(cleanDraw, 1500); 
+    }else if((valueSelected == "👊" && valueComputer == "✌️")||(valueSelected == "✌️" && valueComputer == "🤚")||(valueSelected == "🤚" && valueComputer == "👊")){
         pointPlayer++;
         setTimeout(function() {
             computerIntervalId = setInterval(updateOutputComputer, 333);
-            
         }, 1500);
-        resetRadio();
+        setTimeout(resetRadio, 1500);
         return document.getElementById("playerResult").innerText = pointPlayer;
     }else{
         pointComputer++;
         setTimeout(function() {
             computerIntervalId = setInterval(updateOutputComputer, 333);
-            
         }, 1500);
-        resetRadio();
+        setTimeout(resetRadio, 1500);
         return document.getElementById("computerResult").innerText = pointComputer;
     }
     
@@ -56,8 +56,12 @@ function updateOutputComputer(){
 }
 
 function resetRadio() {
-    let radios = document.querySelectorAll('input[name="option"]');
+    const radios = document.querySelectorAll('input[name="option"]');
     radios.forEach((radio) => {
         radio.checked = false;
     });
+}
+
+function cleanDraw(){
+    document.getElementById("text").innerText = clean;
 }
